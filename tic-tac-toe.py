bba4562835b9a3bdc629a692c167b4a72719f7e6
Dragonappendix <228423547+Dragonappendix@users.noreply.github.com>
2025-09-12 22:34:51 -0700

    Add more skeleton

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,10 +1,34 @@
 def initialize_board():
-    board = [" " for _ in 9]
+    board = [' ' for _ in range(0, 9)]
     return board
 
 
-def display_board(board):
-    print(board[0:3])
-    print(board[3:6])
-    print(board[6:9])
 
+def display_board(board):
+    print(board[0], board[1], board[2])
+    print(board[3], board[4], board[5])
+    print(board[6], board[7], board[8])
+
+
+
+def mark_space(board, space_number, player):
+    pass
+
+
+def check_win_condition():
+    pass
+
+
+def check_space_conflict():
+    pass
+
+def take_turn(board, player):
+    space_number = input("Pick an available space.")
+    check_space_conflict(board, space_number)
+
+
+def play_game():
+    board = initialize_board()
+    display_board(board)
+
+play_game()
